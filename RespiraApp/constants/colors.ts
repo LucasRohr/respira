badb2488f1a3dda69f56fda0dc683aacc14fa38e
LucasRohr <lucasrc17@live.com>
2025-04-27 15:31:58 -0300

    feat: :lipstick: add base global styling files

--- RespiraApp/constants/colors.ts
+++ RespiraApp/constants/colors.ts
@@ -1,1 +1,11 @@
-export const COLORS = Object.freeze({});
+export const COLORS = Object.freeze({
+  primary: "#007FFF",
+  primaryLight: "#4196EC",
+  secondary: "#FBFBF8",
+  background: "#FBFBF8",
+  error: "#D65227",
+  success: "#9FC620",
+  warning: "#E0B520",
+  text: "#3E3C3C",
+  textLight: "#404040",
+});
---
+++ RespiraApp/constants/index.ts
@@ -0,0 +1,3 @@
+export { COLORS } from "./colors";
+export { FONT_SIZES } from "./fonts";
+export { PADDINGS } from "./paddings";

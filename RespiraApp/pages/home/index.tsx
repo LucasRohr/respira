e5baf7aeae535549947b74ba9dcec60a717443f1
LucasRohr <lucasrc17@live.com>
2025-05-07 21:06:58 -0300

    feat: :sparkles: add base map container

--- RespiraApp/pages/home/index.tsx
+++ RespiraApp/pages/home/index.tsx
@@ -1,11 +1,41 @@
-import { View, Text } from "react-native";
+import { View, Text, Platform } from "react-native";
+import { AppleMaps, GoogleMaps } from "expo-maps";
 
 import { styles } from "./styles";
+import { useCallback } from "react";
 
 export const HomePage = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>This is the home screen</Text>
-    </View>
-  );
+  const renderMap = useCallback(() => {
+    const isIOS = Platform.OS == "ios";
+
+    if (isIOS) {
+      return (
+        <AppleMaps.View
+          style={{ width: "auto", height: "100%" }}
+          cameraPosition={{
+            coordinates: {
+              latitude: 51.509865,
+              longitude: -0.1275,
+            },
+            zoom: 1,
+          }}
+        />
+      );
+    }
+
+    return (
+      <GoogleMaps.View
+        style={{ width: "auto", height: "100%" }}
+        cameraPosition={{
+          coordinates: {
+            latitude: 51.509865,
+            longitude: -0.1275,
+          },
+          zoom: 1,
+        }}
+      />
+    );
+  }, []);
+
+  return <View style={styles.container}>{renderMap()}</View>;
 };

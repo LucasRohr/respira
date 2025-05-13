98e573d572749cae2013c65538349450ac615c5c
LucasRohr <lucasrc17@live.com>
2025-05-13 15:19:56 -0300

    feat: :sparkles: add search locations service GET

---
+++ RespiraApp/services/search-locations/index.ts
@@ -0,0 +1,36 @@
+import type {
+  ApiSearchLocationResult,
+  ISearchLocationResult,
+} from "@/interfaces";
+import { searchLocationResultFactory } from "@/factories";
+
+import mockData from "./mock-data.json";
+
+//import { axiosInstance } from "../axios-config";
+
+// Using mocked data due to the fact that the API is not available yet
+export const getSearchLocations = async (
+  search: string
+): Promise<ISearchLocationResult> => {
+  try {
+    // Simulate API delay
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    // const response = await axiosInstance.get<ApiSearchLocationResult>(
+    //   "/search-locations",
+    //   {
+    //     params: {
+    //       search
+    //     },
+    //   }
+    // );
+
+    const mockSearchData: ApiSearchLocationResult = mockData;
+
+    const parsedResponse = searchLocationResultFactory(mockSearchData);
+
+    return parsedResponse;
+  } catch (error) {
+    throw error;
+  }
+};
